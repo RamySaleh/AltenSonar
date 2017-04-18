271916f42158d02e0fa181571787a3e8b9e113ce
RSaleh <Ramy.saleh.me@gmail.com>
2017-04-18 15:54:56 +0200

    Added documentDb repo to get the documents from azure documentDb collection

---
+++ AltenSonar.Infrastructure/Repos/DocumentDBCustomersRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AltenSonar.Core.Entities;
+using AltenSonar.Core.Interfaces;
+using Microsoft.Azure.Documents.Client;
+
+namespace AltenSonar.Infrastructure.Repos
+{
+    public class DocumentDBCustomersRepo : ICustomersRepo
+    {
+        DocumentClient _dbClient;
+        Uri _customersLink;
+
+        public DocumentDBCustomersRepo()
+        {
+            // The database url and a read-only key
+            _dbClient = new DocumentClient(new Uri("https://altensonarserver.documents.azure.com:443/"), "XCEeLTiTbR8TViwqpSwWTbWM2dZ5Y2xwqKeyJ6nmtnFluSLox5ulz5biFt7hLYYE9lwD5TnYeXCBrAd2Sl0iDA==");
+            _customersLink = UriFactory.CreateDocumentCollectionUri("altenSonarDB", "Customers");
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            var customers = _dbClient.CreateDocumentQuery<Customer>(_customersLink).OrderBy(c => c.Name).ToList();
+            return customers;
+        }
+    }
+}
---
+++ AltenSonar.Tests/Repos/DocumentDBRepoTests.cs
@@ -0,0 +1,38 @@
+﻿using AltenSonar.Infrastructure.Repos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltenSonar.Tests.Repos
+{
+    [TestClass]
+    public class DocumentDBRepoTests
+    {
+        [TestMethod]
+        public void DocumentDBCustomersRepoShouldReturnThreeCustomers()
+        {
+            var fakeCustomersRepo = new DocumentDBCustomersRepo();
+
+            var customers = fakeCustomersRepo.GetCustomers();
+
+            var customersListHasThreeCustomers = customers.Count == 3;
+
+            Assert.IsTrue(customersListHasThreeCustomers);
+        }
+
+        [TestMethod]
+        public void DocumentDBCustomersRepoShouldReturnCustomersAndTheirOwnedVehicles()
+        {
+            var fakeCustomersRepo = new DocumentDBCustomersRepo();
+
+            var customers = fakeCustomersRepo.GetCustomers();
+
+            var allCustomersHaveVehicles = customers.All(customer => customer.OwnedVehicles.Count > 0);
+
+            Assert.IsTrue(customers.Count > 0 && allCustomersHaveVehicles);
+        }
+    }
+}
