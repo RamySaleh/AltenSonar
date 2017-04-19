f3e7cac230f238efc45b3ffc50853fe614ad567b
RSaleh <Ramy.saleh.me@gmail.com>
2017-04-19 15:26:30 +0200

    Added some comments on in the scripts

--- AltenSonar/Scripts/dashboard.js
+++ AltenSonar/Scripts/dashboard.js
@@ -1,11 +1,12 @@
-﻿function checkVehiclesConnectionStatus(customers) {
+﻿// Connects to the connection checking wep api to check the status of each vehicle
+function checkVehiclesConnectionStatus(customers) {
     jQuery.support.cors = true;
     $.ajax({
         url: '/api/ConnectionChecker',
         type: 'POST',
         contentType: 'application/json; charset=utf-8',
         data: JSON.stringify(customers),
-        success: function (data) {            
+        success: function (data) {
             updateStatusOnDashboard(data);
         },
         error: function (x, y, z) {
@@ -14,6 +15,7 @@
     });
 }
 
+// Loop on the vehicles and update their statuses on the dashboard
 function updateStatusOnDashboard(customers) {
     var currentVehicle;
     for (var i = 0; i < customers.length; i++) {
@@ -24,16 +26,17 @@ function updateStatusOnDashboard(customers) {
     }
 }
 
+// Updates the status of each vehicle on the dashboard
 function updateVehicleStatusOnDashboard(vehicleId, status) {
     var vehicleStatusCellId = '#' + vehicleId;
-    $(vehicleStatusCellId)[0].className = status ? 'glyphicon glyphicon-ok vehicle-td' : 'glyphicon glyphicon-remove vehicle-td';    
+    $(vehicleStatusCellId)[0].className = status ? 'glyphicon glyphicon-ok vehicle-td' : 'glyphicon glyphicon-remove vehicle-td';
     $(vehicleStatusCellId)[0].setAttribute("data-status", status);
 }
 
+// Filter the documents according to the user selection in the dashboard filters
 function filterCustomers() {
     // Declare variables
-    var input, namefilter, statusFilter, nameCell, statusCell,
-        customersRows, currentCustomerRow, vehiclesRows, currentVehicleRow, isRowEmpty;
+    var input, namefilter, statusFilter, nameCell, statusCell, customersRows, currentCustomerRow, vehiclesRows, currentVehicleRow, isRowEmpty;
 
     input = document.getElementById("txtCustomerNameSearch");
     namefilter = input.value.toUpperCase();
@@ -82,16 +85,19 @@ function filterCustomers() {
             hide(currentCustomerRow);
         }
     }
+}
 
-    function hide(item) {
-        item.style.display = "none";
-    }
+// Hide control
+function hide(item) {
+    item.style.display = "none";
+}
 
-    function show(item) {
-        item.style.display = "";
-    }
+// Show control
+function show(item) {
+    item.style.display = "";
+}
 
-    function stringContains(string, filter) {
-        return nameCell.innerHTML.toUpperCase().indexOf(namefilter) > -1;
-    }
+// Check if a string contains another string
+function stringContains(string, filter) {
+    return string.innerHTML.toUpperCase().indexOf(filter) > - 1;
 }
